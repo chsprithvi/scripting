9692433a9da7188a862d523ab01ab31aa8854900
chsprithvi <30983173+chsprithvi@users.noreply.github.com>
2020-07-15 08:57:25 +0530

    Update and rename EmpUC5.java to EmployeeWagePartFull.java

--- Java Prog 2/EmployeeWagePartFull.java
+++ Java Prog 2/EmployeeWagePartFull.java
@@ -1,4 +1,7 @@
-public class EmpUC5{
+/**
+ * The Employee check class
+ */
+public class EmployeeWagePartFull{
 private static final int IS_FULL_TIME=1;
 private static final int IS_PART_TIME=2;
 private static final int days=20;
@@ -10,6 +13,11 @@ public static void main(String [] args){
    System.out.println("Total Employee Wage :" + wageTotal);
 }
 
+   /**
+	 * Check employee
+	 * @return wage if employee present otherwise false.
+    * @return wage total based upon the employee, if full-time or part-time.
+	 */
 public static void empCheck(){
 
    for (int i=1; i <= days; i++){
@@ -33,6 +41,12 @@ public static void empCheck(){
 }
 }
 }
+   /**
+	 * Calculate employee wage based on params.
+	 * @param ratePerHrs the employee rate per hours.
+	 * @param totalHrs total employee hours.
+	 * @return total employee wage.
+	 */
 public static double cal(final double ratePerHrs, final double totalHrs){
    final double cal= ratePerHrs*totalHrs;
    return cal;
