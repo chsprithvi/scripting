6dee07e4eaf57b8988567ae4b86077581319bee4
chsprithvi <30983173+chsprithvi@users.noreply.github.com>
2020-07-15 08:44:31 +0530

    Update and rename EmpUC1.java to EmployeeCheck.java

--- Java Prog 2/EmployeeCheck.java
+++ Java Prog 2/EmployeeCheck.java
@@ -1,10 +1,16 @@
-public class EmpUC1{
+/**
+ * The Employee check class
+ */
+public class EmployeeCheck{
 private static final int IS_FULL_TIME=1;
 
 public static void main(String [] args){
 	empCheck();
 }
-
+/**
+ * Check employee
+ * @return true if employee present otherwise false.
+ */
 public static void empCheck(){
 double check = Math.floor(Math.random() * 10) % 2;
 	if(IS_FULL_TIME == check){
